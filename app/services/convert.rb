1c980821b3939552bcd2a90aacd790674db28948
Sebastián Palma <vnhnhm@gmail.com>
2020-11-10 23:19:15 +0100

    Add dummy file.

---
+++ app/services/convert.rb
@@ -0,0 +1,15 @@
+# Converts the object into textual markup given a specific `format`
+# (defaults to `:html`)
+#
+# == Parameters:
+# format::
+#   A Symbol declaring the format to convert the object to. This
+#   can be `:text` or `:html`.
+#
+# == Returns:
+# A string representing the object in a specified
+# format.
+#
+def to_format(format = :html)
+  # format the object
+end
